61a76368d54e3733b24cee689c1936c759143f16
Osman Tahir Kuzu <56130302+OsmanTahirKuzu@users.noreply.github.com>
2021-05-12 23:58:19 +0300

    Update marbleWrestling.js

--- marbleWrestling.js
+++ marbleWrestling.js
@@ -8,20 +8,24 @@ var misket = new Image();
 var misket1 = new Image();
 var bg = new Image();
 
-misket.src = "images/misket.png";
-misket1.src = "images/misket1.png";
+misket.src = "images/misket11.png";
+misket1.src = "images/misket22.png";
 bg.src = "images/bg.png";
 
-// some variables(değişecek)
+// some variables
 
-var x1 = 200 ;
-var x2 = 200 ;
+var x1 = 225 ;
+var x2 = 225 ;
 var y1 = 125;
-var y2 = 275;
-var r1, r2 = 12;
+var y2 = 325;
+var r1, r2 = 25;
 
-var score = 0;
-var Score2 = 0;
+var dx = x1 - x2;
+var dy = y1 - y2;
+var mesafe = Math.sqrt(dx * dx + dy * dy);
+
+var score1 = 0;
+var score2 = 0;
 
 document.addEventListener("keydown",hareket);
 function hareket(tus) {
@@ -76,33 +80,60 @@ function hareket(tus) {
 
     }
 }
-function carpisma(x1,x2,){
-    // detect collision
-    var dx = x1 - x2;
-    var dy = y1 - y2;
-    var mesafe = Math.sqrt(dx * dx + dy * dy);
+function carpismaEylemi() {
+    
+    x1 -= dx + 50; 
+    y1 -= dy + 50;
+    x2 += dx + 50;
+    y2 += dy + 50;
+    
+}
 
-    if (mesafe < r1 + r2) {
-        x1+=100;
-        x2+=100;
+function gameOver(){
+   
+    //skor kazanılma yeri
+    if(x1<100 || x1>350 || y1>350 || y1<100){
+        score2++;
+        x1 = 225 ;
+        x2 = 225 ;
+        y1 = 125;
+        y2 = 325;
+    }
+    else if(x2<100 || x2>350 || y2>350 || y2<100){
+        score1++;
+        x1 = 225 ;
+        x2 = 225 ;    
+        y1 = 125; 
+        y2 = 325;    
+    }
+    
+    if(score1 == 3 || score2 == 3){
+        if(score1 ==3){
+            alert("Kazanan 1. oyuncu Skor: "+score1+" "+score2);
+            location.reload(); // reload the page
+        }
+           
+        else if(score2 == 3){
+            alert("Kazanan 2. oyuncu Skor: "+score1+" "+score2);
+            location.reload(); // reload the page
+        }
     }
 }
 // draw images
 function draw(){
     
     ctx.drawImage(bg,100,100);
-    carpisma();
-    
-
     ctx.drawImage(misket,x1,y1);
     ctx.drawImage(misket1,x2,y2);
 
-    ctx.fillStyle = "#000";
-    ctx.font = "20px Verdana";
-    ctx.fillText("P1 : "+score,10,cvs.height-20);
-    ctx.fillText("P2 : "+Score2,425,cvs.height-20);
-    requestAnimationFrame(draw);
-    
-}
+    if(score1 < 3 && score2 < 3){
+        gameOver();
+    }
 
+    if(mesafe < r1 + r2){
+        carpismaEylemi();
+    }
+
+    requestAnimationFrame(draw); 
+}
 draw();
